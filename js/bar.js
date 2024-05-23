d83da49e8258657a7950f7313f0902b58823de61
NinoNineo <papashvili.nino2@gmail.com>
2024-05-23 22:48:48 +0400

    responsive design

---
+++ js/bar.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("openbtn").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+
+if()
+  }
+  
+//   function closeNav() {
+//     document.getElementById("mySidebar").style.width = "0";
+//     document.getElementById("main").style.marginLeft= "0";
+//   }
